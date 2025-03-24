cfacb6adbb6212082e7a8bca4f55abdea31efce9
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-03-24 09:24:17 +0500

    fixed bugs

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import mainimage from '../src/images/main.png'
+import { Component } from 'react'
+ export function App()
+ {
+  const show=()=>{
+      alert('show alert')
+  }
+  return (
+     
+    <div>
+      export default
+      <button  onClick={show}> click</button>
+    </div>
+  )
+}
+
+ export function Main(){
+  return(
+    <div>
+      <h2>export </h2>
+    </div>
+  )
+}
+
+
